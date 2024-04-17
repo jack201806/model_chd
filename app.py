409bc8c44873c27b87918f8b7a1cd71615b555be
thboon3 <thboone96@gmail.com>
2024-04-17 10:59:45 +0200

    Code with 2 models: test

--- app.py
+++ app.py
@@ -19,39 +19,65 @@ import tensorflow as tf
 import gradio as gr
 
 from libreco.data import random_split, DatasetPure, DataInfo
-from libreco.algorithms import LightGCN, UserCF, ItemCF, SVD, SVDpp, NCF, NGCF, ALS
+from libreco.algorithms import LightGCN, UserCF, ItemCF, SVD, SVDpp, NCF, NGCF, ALS, BPR
 from libreco.evaluation import evaluate
 
-data_info = DataInfo.load("model_path", model_name="svdpp_model")
-model = SVDpp.load(
-        path=dir_path+"/model_path", model_name="svdpp_model", data_info=data_info, manual=True
-    )
 
-# Functions
+# Models
 
-def predict(userId, movieId):
+model_paths = {
+    'bpr': './model_path_pure/bpr',
+    'svdpp': './model_path_pure/svdpp',
+    # extra models paths
+}
+
+model_classes = {
+    'bpr': BPR,
+    'svdpp': SVDpp,
+    # extra models
+}
+
+def predict(userId, movieId, modelName):
+    if modelName not in model_paths:
+        raise ValueError("Invalid model name")
+
+    model_path = model_paths[modelName]
+    model_used = model_classes[modelName]
+
+    data_info = DataInfo.load(model_path, model_name=modelName+"_model")
+    model = model_used.load(path=model_path, model_name=modelName+"_model", data_info=data_info, manual=True)
     prediction = model.predict(user=userId, item=movieId)
     return f"{prediction[0]:.1f}"
 
-def recommend(userId, amount):
+def recommend(userId, amount, modelName):
+    if modelName not in model_paths:
+        raise ValueError("Invalid model name")
+
+    model_path = model_paths[modelName]
+    model_used = model_classes[modelName]
+
+    data_info = DataInfo.load(model_path, model_name=modelName+"_model")
+    model = model_used.load(path=model_path, model_name=modelName+"_model", data_info=data_info, manual=True)
     recommendations = model.recommend_user(user=userId, n_rec=amount)
     recommended_items = "\n".join(map(str, recommendations[1]))
     return recommended_items
 
 with gr.Blocks() as interfaces:
     with gr.Row():
+        model_selection = gr.Dropdown(list(model_paths.keys()), label="Select Model")
+    with gr.Row():
         with gr.Column():
             input_userId = gr.Number(label="User ID")
             input_movieId = gr.Number(label="Movie ID")
-            input_rating = gr.Number(label="How many recommendations?")
+            input_recommendations = gr.Number(label="How many recommendations?")
         output_pred = gr.Text(label="Predicted rating:")
         output_rec = gr.Text(label="Recommended items:")
 
-    btn_predict = gr.Button(value="Predict")
-    btn_predict.click(predict, inputs=[input_userId, input_movieId], outputs=[output_pred])
+    btn_predict = gr.Button(value="Predict", visible=True)
+    btn_predict.click(predict, inputs=[input_userId, input_movieId, model_selection], outputs=[output_pred])
 
-    btn_recommend = gr.Button(value="Recommend")
-    btn_recommend.click(recommend, inputs=[input_userId, input_rating], outputs=[output_rec])
+    btn_recommend = gr.Button(value="Recommend", visible=True)
+    btn_recommend.click(recommend, inputs=[input_userId, input_recommendations, model_selection], outputs=[output_rec])
 
 if __name__ == "__main__":
-    interfaces.launch()
+    interfaces.launch() # share=True, auth=("1", "1")
